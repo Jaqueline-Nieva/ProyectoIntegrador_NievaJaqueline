8a269f83b6568067a155becb681d31354a49e19f
Jaqueline Nieva <nievajaqueline@gmail.com>
2022-07-14 22:46:00 -0300

    Avances en el header y seccion acerca de mi

--- ProyectoIntegrador_NievaJaqueline/src/app/app.module.ts
+++ ProyectoIntegrador_NievaJaqueline/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 import { SocialComponent } from './components/social/social.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { BannerComponent } from './components/banner/banner.component';
     HeaderComponent,
     LogoAPComponent,
     SocialComponent,
-    BannerComponent
+    BannerComponent,
+    FooterComponent,
+    AcercaDeComponent
   ],
   imports: [
     BrowserModule,
