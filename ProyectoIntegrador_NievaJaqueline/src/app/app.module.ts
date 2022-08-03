e76f07f53dd1c04bd8a5c339a3032850f5bf12da
Jaqueline Nieva <nievajaqueline@gmail.com>
2022-08-03 14:42:23 -0300

    Seccion proyectos

--- ProyectoIntegrador_NievaJaqueline/src/app/app.module.ts
+++ ProyectoIntegrador_NievaJaqueline/src/app/app.module.ts
@@ -11,6 +11,9 @@ import { FooterComponent } from './components/footer/footer.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 import { EducacionComponent } from './components/educacion/educacion.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { HysComponent } from './components/hys/hys.component';
+import { ProyectoComponent } from './components/proyecto/proyecto.component';
 
 @NgModule({
   declarations: [
@@ -22,11 +25,14 @@ import { EducacionComponent } from './components/educacion/educacion.component';
     FooterComponent,
     AcercaDeComponent,
     ExperienciaComponent,
-    EducacionComponent
+    EducacionComponent,
+    HysComponent,
+    ProyectoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent]
