156bee18447e80ab8a7d74dab3188e54543765c5
Jaqueline Nieva <nievajaqueline@gmail.com>
2022-08-09 20:19:06 -0300

    Arreglos en el front

---
+++ ProyectoIntegrador_NievaJaqueline/BackEnd/src/main/java/com/portfolio/jdn/JdnApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.jdn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdnApplication.class, args);
+	}
+
+}
