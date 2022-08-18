3b7c0214e68478d4dec18d539e794f40d215fe24
Jaqueline Nieva <nievajaqueline@gmail.com>
2022-08-17 22:43:55 -0300

    Conectando el back y el front

---
+++ ProyectoIntegrador_NievaJaqueline/FrontEnd/src/app/service/persona.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PersonaService {
+  URL = 'http://localhost:8080/personas/';
+  constructor() { }
+}
