8ff5a03e49336646c88c8b74ed4f69071ad38688
Jaqueline Nieva <nievajaqueline@gmail.com>
2022-08-18 17:16:58 -0300

    Anexando el back

--- ProyectoIntegrador_NievaJaqueline/FrontEnd/src/app/service/persona.service.ts
+++ ProyectoIntegrador_NievaJaqueline/FrontEnd/src/app/service/persona.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PersonaService {
   URL = 'http://localhost:8080/personas/';
-  constructor() { }
+
+  constructor(private http: HttpClient) { }
+
+  public getPersona(): Observable<persona>{
+    
+  }
 }
