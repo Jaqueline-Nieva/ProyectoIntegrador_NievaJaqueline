47020b6cf37ee43e81fb7f3442841842dcf6c897
Jaqueline Nieva <nievajaqueline@gmail.com>
2022-09-23 21:18:01 -0300

    Quedan cosas por arreglar en jwt

--- ProyectoIntegrador_NievaJaqueline/BackEnd/src/main/java/com/portfolio/jdn/Security/Controller/AuthController.java
+++ ProyectoIntegrador_NievaJaqueline/BackEnd/src/main/java/com/portfolio/jdn/Security/Controller/AuthController.java
@@ -17,6 +17,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -69,8 +73,18 @@ public class AuthController {
         if(bindingResult.hasErrors())
             return new ResponseEntity(new Mensaje("Campos mal puestos"), HttpStatus.BAD_REQUEST);
           
-        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken())
-                    
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+           loginUsuario.getNombreUsuario(), loginUsuario.getPassword()));
+        
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        
+        String jwt = jwtProvider.generateToken(authentication);
+        
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        
+        JwtDto jwtDto = new JwtDto(jwt, userDetails.getUsername(),userDetails.getAuthorities());
+        
+        return new ResponseEntity(jwtDto, HttpStatus.OK);
     }
     
 }
